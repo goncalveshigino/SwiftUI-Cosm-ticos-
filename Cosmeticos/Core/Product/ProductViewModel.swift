2e02a6d868aa551170e8bcff9355de8711144db9
goncalveshigino <goncalveshigino20@gmail.com>
2024-02-05 23:35:07 +0100

    feat: filtrar produtos

--- Cosmeticos/Core/Product/ProductViewModel.swift
+++ Cosmeticos/Core/Product/ProductViewModel.swift
@@ -31,6 +31,16 @@ final class ProductViewModel: ObservableObject {
     
     
     func filterSelected(option: FilterOption) async throws {
+        
+                switch option {
+                case .noFilter:
+                    self.products = try await ProductsManager.shared.getAllProducts()
+                case .priceHigh:
+                    self.products = try await ProductsManager.shared.getAllProductsSortedByPrice(descending: true)
+                case .priceLow:
+                    self.products = try await ProductsManager.shared.getAllProductsSortedByPrice(descending: false)
+                }
+        
         self.selectedFilter = option
         self.getProducts()
     }
